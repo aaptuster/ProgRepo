ed27db9f04662a3efc46badd595b58b170d79b7c
aaptuster <aaptuster@gmail.com>
2016-05-26 20:04:16 +0530

    Fixed the duplicates issue in finding the lines going through points.

--- Misc.java
+++ Misc.java
@@ -17,9 +17,11 @@ public class Misc {
 		// sort this slope array, that holds the information of its point too
 		for (int i = 0; i < pointArr.length-MIN_SEGMENT_LENGTH ; i++)
 		{
-			Slope[] slopeArr = new Slope[pointArr.length-1-i];
-			for (int j = i+1, k=0; j < pointArr.length; j++)
+			Slope[] slopeArr = new Slope[pointArr.length-1];
+			for (int j = 0, k=0; j < pointArr.length; j++)
 			{
+				if ( i == j)
+					continue;
 				slopeArr[k++] =  new Slope(pointArr[i], pointArr[j],pointArr[i].slopeTo(pointArr[j]));
 			}
 			
@@ -33,18 +35,25 @@ public class Misc {
 			int lineLength = 0;
 			for (int x = 0; x < slopeArr.length-1; x++)
 			{
-				if (slopeArr[x].compareTo(slopeArr[x+1]) == 0)
+				if (slopeArr[x].slopeVal == slopeArr[x+1].slopeVal)
 					lineLength++;
 				
 				if (x == slopeArr.length-2 || 
-						slopeArr[x].compareTo(slopeArr[x+1]) != 0)
+						slopeArr[x].slopeVal != slopeArr[x+1].slopeVal)
 				{
-					if (lineLength >= MIN_SEGMENT_LENGTH)
+					if (x == slopeArr.length-2 && slopeArr[x].slopeVal == slopeArr[x+1].slopeVal)
+						x++;
+					
+					if (lineLength >= MIN_SEGMENT_LENGTH-1 && slopeArr[x-lineLength].p2.compareTo(pointArr[i]) == 1 )
 					{
-						System.out.printf("\n line found with length %d and point %s", lineLength, pointArr[i].toString());
+						System.out.printf("\n line found with length %d and point %s with slope %f\n", lineLength+2, pointArr[i].toString(),slopeArr[x-lineLength].slopeVal);
+						System.out.printf("\n %s",slopeArr[x-lineLength].p1.toString());
+						for (int l = x-lineLength; l <= x ; l++)
+							System.out.printf(" ,%s",slopeArr[l].p2.toString());
+							
 						lineCounter++;
-						lineLength = 0;
 					}
+					lineLength = 0;
 				}
 			}
 			
@@ -65,12 +74,12 @@ public class Misc {
 		
 		public int compareTo(Slope s)
 		{
-			if (this.slopeVal > s.slopeVal)
+			if (slopeVal > s.slopeVal)
 				return 1;
-			if (this.slopeVal < s.slopeVal)
+			if (slopeVal < s.slopeVal)
 				return -1;
-			return 0;
-			
+			return 
+					p2.compareTo(s.p2);
 		}
 		
 	}
@@ -78,7 +87,7 @@ public class Misc {
 	public static void main(String[] args)
 	{
 		Point[] pointArr = new Point[args.length/2];
-		for (int i=0, j=0; i < args.length;j++,i++,i++)
+		for (int i=0, j=0; i < args.length ;j++,i++,i++)
 		{
 			pointArr[j] = new Point(Integer.parseInt(args[i]), Integer.parseInt(args[i+1]));
 		}
